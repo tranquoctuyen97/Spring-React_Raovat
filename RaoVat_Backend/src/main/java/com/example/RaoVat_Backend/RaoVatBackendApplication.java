aac006d4498ac335c9680309abc0e7bccf687915
tranquoctuyen97 <tuyentqpd02042@fpt.edu.vn>
2018-10-29 21:11:09 +0700

    innit project

---
+++ RaoVat_Backend/src/main/java/com/example/RaoVat_Backend/RaoVatBackendApplication.java
@@ -0,0 +1,12 @@
+package com.example.RaoVat_Backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RaoVatBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RaoVatBackendApplication.class, args);
+	}
+}
